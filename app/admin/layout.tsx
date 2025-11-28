daa2438333593ecd0b8268c77bf939ca0a0a9cf7
rishisoyal <rishisoyal510@gmail.com>
2025-11-28 11:55:24 +0530

    fix wrong package install

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,10 +1,23 @@
 import Sidebar from "@/components/SideBar";
-
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+	// const token = NextRequest.cookies.get("auth_token")?.value;
+
+ //    if (!token) {
+ //      return redirect("/login");
+ //    }
+
+ //    try {
+ //      await jwtVerify(
+ //        token,
+ //        new TextEncoder().encode(process.env.TOKEN_SECRET)
+ //      );
+ //    } catch {
+ //      return redirect("/login");
+ //    }
   return (
     <>
       <Sidebar />
