37772a9e6a880a65af8bbec4ba09b0885c6692e7
rishisoyal <rishisoyal510@gmail.com>
2025-11-28 15:01:45 +0530

    fix env

--- app/api/proxy/route.ts
+++ app/api/proxy/route.ts
@@ -3,13 +3,16 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
 	const body = await req.text();
 
-	const backendRes = await fetch("http://localhost:8000/api/user/login", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
+	const backendRes = await fetch(
+		`${process.env.NEXT_PUBLIC_BASE_API}/api/user/login`,
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body,
 		},
-		body,
-	});
+	);
 
 	const data = await backendRes.text();
 
