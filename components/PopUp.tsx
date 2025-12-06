958d60b33f3134aaca9cccb546f5630ce368e5d0
rishisoyal <rishisoyal510@gmail.com>
2025-12-06 12:09:14 +0530

    remove close button

--- components/PopUp.tsx
+++ components/PopUp.tsx
@@ -36,14 +36,14 @@ export default function Popup({ isOpen, onClose, children }: Props) {
             >
               {children}
 
-              <div className="w-full flex place-content-center">
+              {/* <div className="w-full flex place-content-center">
                 <button
                   onClick={onClose}
                   className="mt-4 w-24 cursor-pointer py-2 rounded-lg bg-black text-white active:scale-95 hover:scale-105 duration-300"
                 >
                   Close
                 </button>
-              </div>
+              </div> */}
             </div>
           </motion.div>
         </>
